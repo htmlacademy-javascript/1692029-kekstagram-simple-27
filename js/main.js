3b740771dce8f7687243d8242776a5bce54caefc
AnMarss <astanasya@gmail.com>
2022-10-06 22:26:38 +0300

    Линт

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function getRandomPositiveInteger(a, b) {
-    if (a < 0 || b < 0) {
-        return NaN;
-    }
-    const lower = Math.ceil(Math.min (a, b));
-    const upper = Math.floor(Math.max (a, b));
-    const result = Math.random() * (upper - lower + 1) + lower;
-    return Math.floor(result);
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min (a, b));
+  const upper = Math.floor(Math.max (a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
 function checkStringLength (string, length) {
-    const result = string.length <= length;
-    return result;
+  const result = string.length <= length;
+  return result;
 }
 
 getRandomPositiveInteger (1, 0);
