61d8e21dfeb89c88613996f3ab9793eceb737b9e
AnMarss <astanasya@gmail.com>
2022-11-08 19:09:49 +0300

    Эффекты и масштаб

---
+++ js/scale.js
@@ -0,0 +1,48 @@
+const makeSmallerElement = document.querySelector('.scale__control--smaller');
+const makeBiggerElement = document.querySelector('.scale__control--bigger');
+const valueElement = document.querySelector('.scale__control--value');
+const imgDefaultElement = document.querySelector('.img-upload__preview');
+
+const MAX_SCALE_RANGE = 100;
+const MIN_SCALE_RANGE = 25;
+
+let sizeValue = 100;
+
+
+const editSize = () => {
+
+  makeSmallerElement.addEventListener('click', () => {
+    if(sizeValue > MIN_SCALE_RANGE) {
+      sizeValue -= MIN_SCALE_RANGE;
+      valueElement.value = `${sizeValue}%`;
+      imgDefaultElement.style.transform = `scale(${sizeValue / 100})`;
+    }
+  });
+
+  makeBiggerElement.addEventListener('click', () => {
+    if(sizeValue < MAX_SCALE_RANGE) {
+      sizeValue += MIN_SCALE_RANGE;
+      valueElement.value = `${sizeValue}%`;
+      imgDefaultElement.style.transform = `scale(${sizeValue / 100})`;
+    }
+  });
+};
+
+
+const getScaleValue = (value) => {
+  valueElement.value = `${value}%`;
+  imgDefaultElement.style.transform = `scale(${value / 100})`;
+};
+
+getScaleValue(sizeValue);
+
+
+const clearScaleValue = () => {
+  sizeValue = MAX_SCALE_RANGE;
+  getScaleValue(MAX_SCALE_RANGE);
+  imgDefaultElement.style.transform = `scale(${sizeValue / 100})`;
+};
+
+editSize();
+
+export {clearScaleValue};
